efa618a9f7e399d871be6105950071ffee59d7a1
Rodrigo Zapata Sandoval <razapata@uc.cl>
2025-06-10 20:32:58 -0400

    feat: add fibonacci method

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 print("hola")
 
 
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        next_value = fib_sequence[i - 1] + fib_sequence[i - 2]
+        fib_sequence.append(next_value)
+    
+    return fib_sequence
+
 def main():
     print("Hello, World!")
     print("This is a simple Python script.")
     print("It prints a greeting message to the console.")
 
-    
+
 if __name__ == "__main__":
     main()
     print("This script is executed directly.")
